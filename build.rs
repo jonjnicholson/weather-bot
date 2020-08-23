a24c256a681d847bd7ceeb849b9ab78d3a8ab337
Jonathan Nicholson <jon@jnicholson.me>
2020-08-22 20:19:45 -0400

    fix import

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
 use std::fs::File;
+use std::io::Result;
 use std::io::prelude::*;
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<()> {
 	let mut env_file = File::open(".env")?;
     let mut discord_env_var = String::new();
     env_file.read_to_string(&mut discord_env_var)?;
